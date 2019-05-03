f9010643a239d03c19623b0f84ea20f071e9f142
HongSungwoo <sungwoo166@gmail.com>
2019-05-03 17:25:17 +0900

    ProjectStart

---
+++ src/user/UserDAO.java
@@ -0,0 +1,14 @@
+package user;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.sun.jdi.connect.spi.Connection;
+
+public class UserDAO {
+	//ctrl shift o
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+}
