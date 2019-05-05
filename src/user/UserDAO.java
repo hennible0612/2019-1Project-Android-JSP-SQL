03d0ac691b208fb349bf15784cc69d7b66aef465
HongSungwoo <sungwoo166@gmail.com>
2019-05-05 23:38:17 +0900

    jsp 5장까지
    회원가입화면

--- src/user/UserDAO.java
+++ src/user/UserDAO.java
@@ -1,14 +1,49 @@
 package user;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+//회원정보를 불러오거나 저장한다.
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-import com.sun.jdi.connect.spi.Connection;
+
 
 public class UserDAO {
 	//ctrl shift o
-	private Connection conn;
+	private Connection conn; //데이터베이스에 접근하게 해주는 객체
 	private PreparedStatement pstmt;
-	private ResultSet rs;
+	private ResultSet rs; //정보를 저장
 	
+	public UserDAO() { //my sel에 접속하는 부분
+		try{
+			String dbURL = "jdbc:mysql://localhost:3306/BBS"; //데이터베이스에 접속
+			String dbID = "root";
+			String dbPassword = "a12345";
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public int login(String userID, String userPassword) {
+		String SQL = "SELECT userPassword FROM USER WHERE userID= ?";
+		try {
+			pstmt = conn.prepareStatement(SQL); //sql 실횅
+			pstmt.setString(1,userID); //보안하기 위해 사용
+			rs = pstmt.executeQuery();
+			if (rs.next()) { //아이디 있는지 확인
+				if(rs.getString(1).contentEquals(userPassword)) {
+					return 1; //로그인 ㅅ성공
+				}
+				else {
+					return 0; //비밀번호 불일치
+				}
+			}
+			return -1; //아디디가 없음
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return -2; //데이터베이스 오류
+	}
 }
